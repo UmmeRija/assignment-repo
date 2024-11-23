6ba05ef2afecbc54aff175c990eced99b7f9d331
UmmeRija <ummerijababar@gmail.com>
2024-11-22 19:38:32 -0800

    js first assigment

---
+++ js/myself.js
@@ -0,0 +1,10 @@
+let firstName="Umm e Rija Babar"
+let fatherName="Babar Shafi"
+let myAge="20"
+let myQualificaton="FSC"
+
+
+document.write("<h1> My name is " +(firstName)+ "<h1>")
+document.write("<h2> My father name is "+fatherName+"<h2>")
+document.write("<h3> I'm 20 years old "+myAge+"<h3>")
+document.write("<h4> I have completed my "+myQualificaton+" in 2024 <h4> in 2024")
