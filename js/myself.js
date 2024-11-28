e35d2cdae630a9818fbf8598fcffbdffefc54d80
UmmeRija <ummerijababar@gmail.com>
2024-11-27 21:11:58 -0800

    js 2nd assigment

--- js/myself.js
+++ js/myself.js
@@ -8,3 +8,11 @@ document.write("<h1> My name is " +(firstName)+ "<h1>")
 document.write("<h2> My father name is "+fatherName+"<h2>")
 document.write("<h3> I'm 20 years old "+myAge+"<h3>")
 document.write("<h4> I have completed my "+myQualificaton+" in 2024 <h4> in 2024")
+
+
+prompt ("Enter your name", "Name")
+prompt ("Enter your Father name", " Father Name")
+prompt ("Enter your Age", "Age")
+prompt ("Enter your Qualification", "Qualification")
+
+alert("welcome here")
