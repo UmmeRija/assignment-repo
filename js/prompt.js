f86f9bb35bf6b438cc62a25263b507152f60e58f
UmmeRija <ummerijababar@gmail.com>
2024-12-03 16:13:18 +0500

    document

--- js/prompt.js
+++ js/prompt.js
@@ -1,7 +1,9 @@
- let a = prompt ("Enter your name", "Name")
- let b = prompt ("Enter your Father name", " Father Name")
- let c = prompt ("Enter your Age", "Age")
- let d = prompt ("Enter your Qualification", "Qualification")
+ let a = document.getById("name");
+ let b = document.getById ("Enter your Father name", " Father Name")
+ let c = document.getById  ("Enter your Age", "Age")
+ let d = document.getById  ("Enter your Qualification", "Qualification")
+
+ console.log(a)
 
 // alert("welcome here")
 if(a !== "" && b !== "" && c !== "" && d !== ""){
@@ -11,8 +13,8 @@ if(a !== "" && b !== "" && c !== "" && d !== ""){
    location.assign("prompt.html")
 }
 
-document.write ("<h1 style='color: deeppink ;'> Your Name is "+(a)+ "<h1>")
-document.write("<h2 style='color: rgb(255, 62, 165);'> Your  Father Name is " +(b)+ "<h2>")
-document.write("<h3 style='color: rgb(255, 132, 197) ;'> Your Age is" +(c)+"<h3>")
-document.write("<h4 style='color: rgb(255, 169, 215);'> Your Qualification is "+(d)+ "<h4>")
-document.body.style.background = "rgb(253, 223, 244)";
+// document.write ("<h1 style='color: deeppink ;'> Your Name is "+(a)+ "<h1>")
+// document.write("<h2 style='color: rgb(255, 62, 165);'> Your  Father Name is " +(b)+ "<h2>")
+// document.write("<h3 style='color: rgb(255, 132, 197) ;'> Your Age is" +(c)+"<h3>")
+// document.write("<h4 style='color: rgb(255, 169, 215);'> Your Qualification is "+(d)+ "<h4>")
+// document.body.style.background = "rgb(253, 223, 244)";
